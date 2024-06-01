b6985398c555323d8761db2bf85f1111801517f1
Omri Cohen <omricohen19@gmail.com>
2024-06-01 17:54:31 +0300

    moved to React

--- src/video_screen/VideoScreen.js
+++ src/video_screen/VideoScreen.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import './VideoScreen.css';
 
 const VideoScreen = () => {
     return (
-      <div>
-        <h1>VideoScreen</h1>
-        <p>copy and paste the page code here</p>
-      </div>
+      <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2 bg-light vh-100">
+                      <ul class="list-group">
+                        <li class="list-group-item">An active item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">A fourth item</li>
+                        <li class="list-group-item">And a fifth one</li>
+                      </ul>
+                </div>
+                <div class="col-7">
+                    <div id="searchBar">
+                        <input></input>
+                        <button>Search</button>
+                    </div>
+                    <div>
+                        <div>
+                            <img class="thumbnail" src="maxresdefault.jpg" alt=''></img>
+                            <div id="title">This is the best Video Title</div>
+                            <div class="container">
+                                <div class="userBar">
+                                    <img src="lebron.png" class="img-thumbnail" alt=''></img>
+                                    <a id="userName" href="">User Name</a>
+                                    <button class="userBar-button">Subscribe</button>
+                                </div>
+                                <div class="actionBar">
+                                    <button class="userBar-button">Like</button>
+                                    <button class="userBar-button">Dislike</button>
+                                    <button class="userBar-button">Share</button>
+                                </div>
+                            </div>
+                            <div id="description">
+                                    <div id="stats">100M views - 1 day ago</div>
+                                    <div>this is the description</div>
+                            </div>
+                            <div>
+                                This is the comment section
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+                <div class="col-3">
+                  column
+                </div>
+            </div>    
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+      </body>
     );
   };
 
