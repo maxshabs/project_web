7cc8a0aa5516c465ca7e109c0f3942dc04bdf2fe
Omri Cohen <omricohen19@gmail.com>
2024-06-01 13:07:45 +0300

    first commit to the branch

---
+++ src/video_screen/VideoScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoScreen = () => {
+    return (
+      <div>
+        <h1>VideoScreen</h1>
+        <p>copy and paste the page code here</p>
+      </div>
+    );
+  };
+
+export default VideoScreen
