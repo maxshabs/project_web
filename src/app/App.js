ffe1eee49b3e886e5f0ab4abea373621a4425cf7
maxshabs <maxshabs2@gmail.com>
2024-06-04 22:07:03 +0300

    Push after merge

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import ManageRoutes from './ManageRoutes';
+import ManageRoutes from '../ManageRoutes';
 
 function App() {
   return (
--- src/logged_in_header/LoggedInHeader.js
+++ src/logged_in_header/LoggedInHeader.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './LoggedInHeader.module.css';
+import SignInButton from '../signInButton/SignInButton';
 
 const LoggedInHeader = ({ loggedInUser }) => {
 
