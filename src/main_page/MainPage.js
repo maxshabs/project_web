5711f15ca975435f1c024a8672bb71e329eadfa0
maxshabs <maxshabs2@gmail.com>
2024-06-04 23:29:55 +0300

    Search bar update

--- src/main_page/MainPage.js
+++ src/main_page/MainPage.js
@@ -1,13 +1,8 @@
 import './MainPage.css';
 import '../theme.css';
-import videos from '../data/videos.json';
 import LeftMenu from '../leftMenu/LeftMenu';
-import Search from '../search/Search';
 import VideoListResults from '../videoListResults/VideoListResults';
-import SignInButton from '../signInButton/SignInButton';
-import UploadButton from '../uploadButton/UploadButton';
 import CategoryButton from '../categoryButton/CategoryButton';
-import ThemeSwitchButton from '../themeSwitchButton/ThemeSwitchButton';
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
@@ -20,16 +15,6 @@ function MainPage({ videos, theme, doSearch, toggleTheme}) {
                         <LeftMenu />
                     </div>
                     <div className="col-11 main-content">
-                        <div className="row align-items-center">
-                            <div className="col d-flex justify-content-center">
-                                <Search doSearch={doSearch} />
-                            </div>
-                            <div className="col-auto d-flex justify-content-center">
-                                <ThemeSwitchButton toggleTheme={toggleTheme} theme={theme} />
-                                <UploadButton />
-                                <SignInButton />
-                            </div>
-                        </div>
                         <div className="categories row d-flex justify-content-center">
                             <row>
                                 <CategoryButton category="All" />
