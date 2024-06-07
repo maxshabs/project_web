f8440bd89afa45a5ed05a3b7d2a3c709d1eaea63
Omri Cohen <omricohen19@gmail.com>
2024-06-07 12:42:33 +0300

    added new videos

--- src/video_screen/SideVideo/SideVideo.js
+++ src/video_screen/SideVideo/SideVideo.js
@@ -5,17 +5,19 @@ import './SideVideo.css';
 function SideVideo({ id, title, author, views, img, date }) {
   return (
     <Link to={`/videos/${id}`} className="sideVideoContainer link-style">
+      <div className='side-video-container'>
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-4">
           <img src={img} className="sideVideoPic" alt=""></img>
         </div>
-        <div className="col-md-6">
+        <div className="col-md-8">
           <div className="sideVideoTextContainer">
             <h5 className="sideVideoTitle">{title}</h5>
             <p className="sideVideoText"> {author}</p>
             <p className="sideVideoStats"><small className="text-body-secondary">{views} views - {date} ago</small></p>
           </div>
         </div>
+      </div>
       </div>
     </Link>
   );
